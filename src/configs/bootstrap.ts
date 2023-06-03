f486c4ba4e32a3720ef94a3c60c046fe26e79cb4
alobe <alobef0515@gmail.com>
2023-06-03 19:44:56 +0800

    feat: dir adjust

--- src/configs/bootstrap.ts
+++ src/configs/bootstrap.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 // "before" will trigger before the app lift.
 export const bootstrapBefore = (): object => {
   // solve ncc path link.
-  const result = dotenv.config({ path: join(__dirname, '../.env') })
+  const result = dotenv.config({ path: join(process.cwd(), './.env') })
   if (result.error) {
     print.danger('Environment variable not loaded: not found ".env" file.')
     return {}
